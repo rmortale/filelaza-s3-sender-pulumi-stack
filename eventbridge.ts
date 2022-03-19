0f611c0959b643768a2dde2fb16d206901c25992
Antonio Dolce <dolce.antonio@gmail.com>
2022-03-19 23:15:24 +0100

    update event rule

--- eventbridge.ts
+++ eventbridge.ts
@@ -10,15 +10,17 @@ const s3BucketName = uploadStack.getOutput("s3bucket");
 
 const s3objCreated = new aws.cloudwatch.EventRule(`${prefix}-s3ObjectCreated`, {
     description: "Capture each S3 object created event",
-    eventPattern: JSON.stringify({
-        "source": ["aws.s3"],
-        "detail-type": ["Object Created"],
-        "detail": {
-            "bucket": {
-                "name": [s3BucketName]
+    eventPattern: s3BucketName.apply(name =>
+        JSON.stringify({
+            "source": ["aws.s3"],
+            "detail-type": ["Object Created"],
+            "detail": {
+                "bucket": {
+                    "name": [name]
+                }
             }
-        }
-    })
+        })
+    )
 });
 
 export const eventrule = s3objCreated;
